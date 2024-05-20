50a405920feda7a9a73cffec90b3c6f33b2fa805
Abinntr <muhammadbintoro9@gmail.com>
2024-05-20 10:12:59 +0700

    nambahin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,27 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/r', 'login::index');
+$routes->get('/', 'login::index');
 
-$routes->get('/a', 'Daftar::index');
+$routes->get('/index', 'Index::index');
+
+$routes->get('/daftar', 'Daftar::index');
+
+$routes->get('/', 'index::index');
+
+$routes->get('/', 'cart::index');
+
+$routes->get('/', 'checkout::index');
+
+$routes->get('/', 'shop::index');
+
+$routes->get('/', 'single-product::index');
+$routes->setAutoroute(true);
+
+
+
+
+
+
+
+
