8787cc16c5479f5df409d59324a1ceeb5d0f4e28
Abinntr <muhammadbintoro9@gmail.com>
2024-05-27 00:14:28 +0700

    initial

--- app/Controllers/user.php
+++ app/Controllers/user.php
@@ -1,1 +1,59 @@
- 
+<?php
+
+namespace App\Controllers;
+
+use App\Models\UserModel;
+use CodeIgniter\Controller;
+
+class User extends Controller
+{
+    public function index()
+    {
+        $model = new UserModel();
+        $data['user'] = $model->findAll();
+        return view('user/index', $data);
+    }
+
+    public function create()
+    {
+        return view('user/create');
+    }
+
+    public function store()
+    {
+        $model = new UserModel();
+        $data = [
+            'username' => $this->request->getPost('username'),
+            'email'    => $this->request->getPost('email'),
+            'password' => $this->request->getPost('password')
+        ];
+        $model->insert($data);
+        return redirect()->to('/users');
+    }
+
+    public function edit($id)
+    {
+        $model = new UserModel();
+        $data['user'] = $model->find($id);
+        return view('users/edit', $data);
+    }
+
+    public function update($id)
+    {
+        $model = new UserModel();
+        $data = [
+            'username' => $this->request->getPost('username'),
+            'email'    => $this->request->getPost('email'),
+            'password' => $this->request->getPost('password')
+        ];
+        $model->update($id, $data);
+        return redirect()->to('/users');
+    }
+
+    public function delete($id)
+    {
+        $model = new UserModel();
+        $model->delete($id);
+        return redirect()->to('/user');
+    }
+}
