cf7d262baed6203cf1dab72886ed77947cf15d7e
via ntiek <salmaumaynaayman@gmail.com>
2024-05-26 15:46:09 +0700

    jalanin fungsi login

---
+++ app/Models/loginuser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class loginuser extends Model
+{
+public function loginUser($email, $password)
+{
+    return $this->db->table('tb_user')
+    ->where([
+        'email' => $email,
+        'password' => $password,
+    ])->get()->getRowArray();
+}
+}
